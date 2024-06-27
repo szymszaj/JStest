0a6e20a259fa2d32ce53a9e1abc2c0b1cfdf5e0d
Szymon Zych <szymonzych868@gmail.com>
2024-06-27 20:20:47 +0200

    update the file

--- javascript-practice/objects/examples.js
+++ javascript-practice/objects/examples.js
@@ -12,9 +12,34 @@ const dogAge = 6;
 const hobby = "gnaw wood";
 
 const dog = {
-  // name: dogName,
-  // age: dogAge,
   dogName,
   dogAge,
   hobby,
 };
+
+const user = {
+  name: "Simon",
+  lastName: "Oxl",
+  age: 55,
+  hobby: "cars",
+  job: "car mechanict",
+  showName() {
+    console.log(this.name, this.age);
+  },
+};
+user.showName();
+
+class User {
+  constructor(name, age, job) {
+    this.name = name;
+    this.age = age;
+    this.job = job;
+
+    this.hello = function () {
+      console.log(`Hi ${this.name}I like my job and I work in ${this.job}`);
+    };
+  }
+}
+const firstUser = new User("Lily", 32, "boss");
+
+firstUser.hello();
