2f4792664d96aeb92dd507d7cc2a297220382c68
Szymon Zych <szymonzych868@gmail.com>
2024-07-05 20:53:45 +0200

    arrays

--- javascript-practice/arrays/app.js
+++ javascript-practice/arrays/app.js
@@ -10,3 +10,10 @@ fruits.forEach(function (fruit) {
 for (let fruit of fruits) {
   console.log(fruit);
 }
+let cars = ["BMW", "Audi", "Mercedes"];
+for (let i = 0; i < cars.length; i++) {
+  console.log(cars[i]);
+}
+cars.forEach(function (car) {
+  console.log(car);
+});
