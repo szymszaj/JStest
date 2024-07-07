85577381fd74bc03403ba7fce32ee0cae416d682
Szymon Zych <szymonzych868@gmail.com>
2024-07-07 22:48:28 +0200

    filtering data in a loop

--- javascript-practice/loops/for.js
+++ javascript-practice/loops/for.js
@@ -24,3 +24,25 @@ for (let x = 0; x <= 45; x++) {
     console.log("%c" + x, "color: red");
   }
 }
+
+
+const books = [
+  { title: "Harry Potter", author: "J.K. Rowling", read: true },
+  { title: "Wiedźmin", author: "Andrzej Sapkowski", read: false },
+  { title: "Hobbit", author: "J.R.R. Tolkien", read: true },
+  { title: "Gra o tron", author: "George R.R. Martin", read: false }
+];
+
+for (const { title, author, read } of books) {
+
+  if (!read) continue;
+
+  console.log(`Przeczytana książka: "${title}" autorstwa ${author}`);
+}
+
+const readBooksTitles = [];
+for (const { title, read } of books) {
+  if (read) readBooksTitles.push(title);
+}
+
+console.log("Tytuły przeczytanych książek:", readBooksTitles);
