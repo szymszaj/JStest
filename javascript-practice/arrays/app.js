29e9bfeaba6618b04c4bcc1b0bc7cc733032c9ce
Szymon Zych <szymonzych868@gmail.com>
2024-07-10 23:53:34 +0200

    .

--- javascript-practice/arrays/app.js
+++ javascript-practice/arrays/app.js
@@ -17,3 +17,11 @@ for (let i = 0; i < cars.length; i++) {
 cars.forEach(function (car) {
   console.log(car);
 });
+
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+let squaredNumbers = numbers.map((num) => num * num);
+console.log(squaredNumbers);
+
+let evenNumbers = numbers.filter((num) => num % 2 === 0);
+console.log(evenNumbers);
