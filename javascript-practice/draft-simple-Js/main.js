51fa449f0ef5b2a423f641a79f3c3c1775dcd17d
Szymon Zych <szymonzych868@gmail.com>
2024-07-11 23:15:43 +0200

    chore: Add destructuring assignment for person object

--- javascript-practice/draft-simple-Js/main.js
+++ javascript-practice/draft-simple-Js/main.js
@@ -2,3 +2,7 @@ function addition(a, b) {
   return a + b;
 }
 const addition = (a, b) => a + b;
+
+const person = { name: "Alice", age: 22 };
+const { name, age } = person;
+console.log("Name: " + name + ", Age: " + age);
