af20ccae78739411c47fed0b56876b0ff1fa3bbb
Szymon Zych <szymonzych868@gmail.com>
2024-07-12 22:07:28 +0200

    feat: Add multiplication function and examples

--- javascript-practice/draft-simple-Js/main.js
+++ javascript-practice/draft-simple-Js/main.js
@@ -10,3 +10,17 @@ console.log("Name: " + name + ", Age: " + age);
 const name1 = "Jone";
 const greeting = `Hello ${name1}!`;
 console.log(greeting);
+
+function multiply(x, y) {
+  return x * y;
+}
+const multiplyArrow = (x, y) => x * y;
+
+const book = { title: "The Great Gatsby", author: "F. Scott Fitzgerald" };
+const { title, author } = book;
+console.log(`Title: ${title}, Author: ${author}`);
+
+const hoursWorked = 40;
+const hourlyRate = 25;
+const weeklyEarnings = `Total earnings: $${hoursWorked * hourlyRate}`;
+console.log(weeklyEarnings);
