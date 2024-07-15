e9388652ed6eaab7a9a876736d9bcecf3981dab8
Szymon Zych <szymonzych868@gmail.com>
2024-07-15 19:54:59 +0200

    feat: Add default parameter example for introduce function

--- javascript-practice/draft-simple-Js/defaultParameters.js
+++ javascript-practice/draft-simple-Js/defaultParameters.js
@@ -3,3 +3,9 @@ const greet = (name = "Szymon") => {
 };
 
 console.log(greet("Szymon"));
+
+const introduce = (name = "Szymon", age = 30, hobby = "programming") => {
+  return `Hello, my name is ${name}, I am ${age} years old and I enjoy ${hobby}.`;
+};
+
+console.log(introduce("Szymon", 30, "programming"));
