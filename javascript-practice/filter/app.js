a04af6334f2e7b6889e99f4d9ba62cb8d380449e
Szymon Zych <szymonzych868@gmail.com>
2024-07-23 22:10:33 +0200

    feat: Add filter function to get even numbers from array

--- javascript-practice/filter/app.js
+++ javascript-practice/filter/app.js
@@ -4,3 +4,13 @@ const greaterThanFive = numbers.filter((num) => num > 5);
 
 console.log(evens);
 console.log(greaterThanFive);
+
+const numbers1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+function isEven(number) {
+  return number % 2 === 0;
+}
+
+const evenNumbers = numbers.filter(isEven);
+
+console.log(evenNumbers);
