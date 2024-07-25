02de3fc9878038312ab70bd45fe0f63558627b12
Szymon Zych <szymonzych868@gmail.com>
2024-07-25 23:50:16 +0200

    feat: update code

--- javascript-practice/loops/for.js
+++ javascript-practice/loops/for.js
@@ -25,16 +25,14 @@ for (let x = 0; x <= 45; x++) {
   }
 }
 
-
 const books = [
   { title: "Harry Potter", author: "J.K. Rowling", read: true },
   { title: "Wiedźmin", author: "Andrzej Sapkowski", read: false },
   { title: "Hobbit", author: "J.R.R. Tolkien", read: true },
-  { title: "Gra o tron", author: "George R.R. Martin", read: false }
+  { title: "Gra o tron", author: "George R.R. Martin", read: false },
 ];
 
 for (const { title, author, read } of books) {
-
   if (!read) continue;
 
   console.log(`Przeczytana książka: "${title}" autorstwa ${author}`);
