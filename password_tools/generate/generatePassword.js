ff149e754c503e57d23cf0aae954d266ad50658a
Szymon Zych <szymon.zych@monogo.pl>
2024-07-26 23:51:46 +0200

    chore: Refactor generatePassword function for readability and maintainability

--- password_tools/generate/generatePassword.js
+++ password_tools/generate/generatePassword.js
@@ -1,15 +1,16 @@
 function generatePassword(length) {
-  const charset = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()';
-  let password = '';
-  
+  const charset =
+    "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()";
+  let password = "";
+
   for (let i = 0; i < length; i++) {
     const randomIndex = Math.floor(Math.random() * charset.length);
     password += charset[randomIndex];
   }
-  
+
   return password;
 }
 
-const passwordLength = 10; // Change this to the desired length of the password
+const passwordLength = 10;
 const generatedPassword = generatePassword(passwordLength);
 console.log(generatedPassword);
