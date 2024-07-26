c04bc6e3e699ff067d252828835d9974413cd765
Szymon Zych <szymon.zych@monogo.pl>
2024-07-26 23:45:58 +0200

    feat: Add generatePassword function for generating random passwords

---
+++ password_tools/generate/generatePassword.js
@@ -0,0 +1,15 @@
+function generatePassword(length) {
+  const charset = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()';
+  let password = '';
+  
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * charset.length);
+    password += charset[randomIndex];
+  }
+  
+  return password;
+}
+
+const passwordLength = 10; // Change this to the desired length of the password
+const generatedPassword = generatePassword(passwordLength);
+console.log(generatedPassword);
