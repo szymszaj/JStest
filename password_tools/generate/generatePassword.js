8ebd8ab8cf2de266497f3003aafd2a0160478687
Szymon Zych <szymon.zych@monogo.pl>
2024-07-28 21:06:04 +0200

    feat: Refactor generatePassword function for improved readability and maintainability

--- password_tools/generate/generatePassword.js
+++ password_tools/generate/generatePassword.js
@@ -1,16 +1,25 @@
-function generatePassword(length) {
+function generatePassword() {
+  const length = 14;
   const charset =
-    "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()";
+    "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+~`|}{[]:;?><,./-=";
   let password = "";
-
-  for (let i = 0; i < length; i++) {
-    const randomIndex = Math.floor(Math.random() * charset.length);
-    password += charset[randomIndex];
+  for (let i = 0, n = charset.length; i < length; ++i) {
+    password += charset.charAt(Math.floor(Math.random() * n));
   }
-
   return password;
 }
 
-const passwordLength = 10;
-const generatedPassword = generatePassword(passwordLength);
-console.log(generatedPassword);
+function generateUniquePassword(existingPasswords) {
+  let password;
+  do {
+    password = generatePassword();
+  } while (existingPasswords.has(password));
+  existingPasswords.add(password);
+  return password;
+}
+
+const existingPasswords = new Set();
+for (let i = 0; i < 5; i++) {
+  const newPassword = generateUniquePassword(existingPasswords);
+  console.log(newPassword);
+}
