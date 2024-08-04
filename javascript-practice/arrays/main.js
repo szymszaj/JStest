ba5c04b5c8d2d0681b5f0250881935ee5d11a587
Szymon Zych <szymonzych868@gmail.com>
2024-08-04 23:43:35 +0200

    refactor: Modify main.js to filter and modify integers in myArray

--- javascript-practice/arrays/main.js
+++ javascript-practice/arrays/main.js
@@ -12,13 +12,36 @@ const myArray = [
   },
   new Date("2023-01-01"),
   42,
+  1500,
   "world",
 ];
+
 const integers = myArray.filter(
   (item) => typeof item === "number" && Number.isInteger(item)
 );
+
 console.log(integers);
 
 myArray.forEach((item, index) => {
+  console.log(`Element at index ${index}:`, item);
+});
+
+function modifyIntegers(array) {
+  return array.map((item) => {
+    if (typeof item === "number" && Number.isInteger(item)) {
+      if (item < 20) {
+        return item * 2;
+      } else if (item > 1000) {
+        return 1000;
+      }
+    }
+    return item;
+  });
+}
+const modifiedArray = modifyIntegers(myArray);
+
+console.log("Modified Array:", modifiedArray);
+
+modifiedArray.forEach((item, index) => {
   console.log(`Element at index ${index}:`, item);
 });
