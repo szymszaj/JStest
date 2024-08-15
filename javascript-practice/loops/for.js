6f2e8b6d3c0ad1a0cc4ea3f73ef250b18928c342
Szymon Zych <szymonzych868@gmail.com>
2024-08-15 21:43:10 +0200

    feat: Add code to check if numbers in array are even or odd

--- javascript-practice/loops/for.js
+++ javascript-practice/loops/for.js
@@ -65,3 +65,13 @@ for (let i = 0; i < books.length; i++) {
 }
 
 console.log("Tytuły przeczytanych książek:", readBooksTitles);
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+for (let i = 0; i < numbers.length; i++) {
+  if (numbers[i] % 2 === 0) {
+    console.log(`${numbers[i]} is even`);
+  } else {
+    console.log(`${numbers[i]} is odd`);
+  }
+}
