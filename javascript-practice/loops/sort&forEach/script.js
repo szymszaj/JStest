b8868f299b8889cbc4dff9217b31e7b037cfc3ad
Szymon Zych <szymonzych868@gmail.com>
2024-08-19 19:31:57 +0200

    refactor: Add sort method to hotels array

---
+++ javascript-practice/loops/sort&forEach/script.js
@@ -0,0 +1,8 @@
+const hotels = [
+  { city: "Warsaw", price: 350 },
+  { city: "Krakow", price: 300 },
+  { city: "Gdansk", price: 250 },
+  { city: "Wroclaw", price: 400 },
+  { city: "Poznan", price: 320 },
+  { city: "Sopot", price: 500 },
+];
