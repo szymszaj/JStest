7f8251b515b8daad237e5a3302f6ec2f3b053f86
Szymon Zych <szymonzych868@gmail.com>
2024-08-20 23:13:24 +0200

    refactor: Add Person and User classes and create instances

--- javascript-practice/objects/examples.js
+++ javascript-practice/objects/examples.js
@@ -55,3 +55,41 @@ class Person {
   }
 }
 const person1 = new Person("Simon", 33, "developer");
+
+class Person {
+  constructor(name, age, job) {
+    this.name = name;
+    this.age = age;
+    this.job = job;
+  }
+
+  showName() {
+    console.log(this.name);
+  }
+}
+
+class User {
+  constructor(name, age, job) {
+    this.name = name;
+    this.age = age;
+    this.job = job;
+  }
+
+  hello() {
+    console.log(`Hi ${this.name}, I like my job and I work in ${this.job}`);
+  }
+}
+
+const person1 = new Person("Simon", 33, "developer");
+
+const user1 = new User("Lily", 32, "boss");
+
+const combinedObject = {
+  ...person1,
+  ...user1,
+  showName: person1.showName,
+  hello: user1.hello,
+};
+
+combinedObject.showName();
+combinedObject.hello();
