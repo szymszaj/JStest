2458e6942c9e47c3f85567efe28327aec6dfe73c
Szymon Zych <szymonzych868@gmail.com>
2024-08-25 15:43:39 +0200

    refactor: Add job property to exampleUser array elements

--- javascript-practice/practical-js-examples/map-transformation.js
+++ javascript-practice/practical-js-examples/map-transformation.js
@@ -1,12 +1,14 @@
 const exampleUser = [
-  { id: 55, name: "Alice", age: 25 },
-  { id: 33, name: "Bob", age: 30 },
-  { id: 351, name: "Charlie", age: 35 },
+  { id: 55, name: "Alice", age: 25, job: "developer" },
+  { id: 33, name: "Bob", age: 30, job: "design" },
+  { id: 351, name: "Charlie", age: 35, job: "manager" },
+  { id: 21, name: "David", age: 40, job: "developer" },
   { id: 21, name: "David", age: 40 },
 ];
 const userNames = exampleUser.map((user) => ({
   userId: user.id,
-  // userName: user.name,
+  userName: user.name,
   useAge: user.age,
+  userJob: user.job || "No job",
 }));
 console.log(userNames);
