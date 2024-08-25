2d53ca20d9537c6a9172d766c3e1fe78e25b3309
Szymon Zych <szymonzych868@gmail.com>
2024-08-25 15:43:43 +0200

    refactor: Add filter for older users in filter-find.js

--- javascript-practice/practical-js-examples/filter-find.js
+++ javascript-practice/practical-js-examples/filter-find.js
@@ -3,3 +3,9 @@ const users = [
   { id: 2, name: "Bob", age: 30 },
   { id: 3, name: "Charlie", age: 35 },
 ];
+const olderUser = users.filter((user) => user.age > 27);
+console.log(olderUser);
+
+const olderUser1 = users.filter((user) => user.age > 30);
+console.log(olderUser1);
+
