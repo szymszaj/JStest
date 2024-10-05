25e12521ffa085d9182eaa69d9e06a7a162243a7
Szymon Zych <szymonzych868@gmail.com>
2024-10-05 23:52:44 +0200

    Refactor

--- javascript-practice/simple-functions/symbol-type/value.js
+++ javascript-practice/simple-functions/symbol-type/value.js
@@ -1,10 +1,15 @@
-let globalSym = Symbol.for("name");
+const globalSym = Symbol.for("name");
 
-let anotherObj = {
+const anotherObj = {
   [globalSym]: "Global symbol value",
 };
 
 console.log(anotherObj[globalSym]);
-let sameSym = Symbol.for("globalSymbol");
+
+const sameSym = Symbol.for("globalSymbol");
 
 console.log(globalSym === sameSym);
+
+const sameGlobalSym = Symbol.for("name");
+
+console.log(globalSym === sameGlobalSym);
