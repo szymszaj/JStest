669b7dfbaac053ffd605ab96e81f2d77f186cc64
Szymon Zych <szymonzych868@gmail.com>
2024-10-05 23:50:55 +0200

    Refactor symbol type functionality and add example usage

--- javascript-practice/simple-functions/symbol-type/app.js
+++ javascript-practice/simple-functions/symbol-type/app.js
@@ -9,3 +9,15 @@ let obj = {
 console.log(obj[sym1]);
 console.log(obj[sym2]);
 console.log(sym1 === sym2);
+
+let symA = Symbol("uniqueKey");
+let symB = Symbol("uniqueKey");
+
+let anotherObj = {
+  [symA]: "Value for symA",
+  [symB]: "Value for symB",
+};
+
+console.log(anotherObj[symA]);
+console.log(anotherObj[symB]);
+console.log(symA === symB);
