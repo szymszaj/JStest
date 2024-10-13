e9fc6d4d1caf6b08eec665d1b826ac7f6af4b12f
Szymon Zych <szymonzych868@gmail.com>
2024-10-13 23:41:25 +0200

    Refactor symbol type functionality and add example usage

--- javascript-practice/simple-functions/symbol-type/value.js
+++ javascript-practice/simple-functions/symbol-type/value.js
@@ -10,6 +10,33 @@ const sameSym = Symbol.for("globalSymbol");
 
 console.log(globalSym === sameSym);
 
+const sameGlobalSym1 = Symbol.for("name");
+
+console.log(globalSym === sameGlobalSym);
+
+const globalSym1 = Symbol.for("name");
+
+const advancedObj = {
+  [globalSym]: "Global symbol value",
+  localSym: Symbol("local"),
+  [Symbol.iterator]: function* () {
+    yield this[globalSym];
+    yield this.localSym.description;
+  },
+  displaySymbols: function () {
+    console.log(`Global Symbol: ${this[globalSym]}`);
+    console.log(`Local Symbol: ${this.localSym.description}`);
+  },
+};
+
+console.log(advancedObj[globalSym]);
+
 const sameGlobalSym = Symbol.for("name");
 
 console.log(globalSym === sameGlobalSym);
+
+advancedObj.displaySymbols();
+
+for (let value of advancedObj) {
+  console.log(value);
+}
