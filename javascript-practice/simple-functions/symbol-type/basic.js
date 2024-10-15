31e5084a342e0b560f4408afcf3c1c49e2f8e87f
Szymon Zych <szymonzych868@gmail.com>
2024-10-15 22:16:24 +0200

    Add symbol type functionality and example usage

--- javascript-practice/simple-functions/symbol-type/basic.js
+++ javascript-practice/simple-functions/symbol-type/basic.js
@@ -11,3 +11,20 @@ console.log(car.model);
 console.log(car[engineId]);
 
 console.log(car["engineId"]);
+
+let calculateSecretValue = Symbol("calculateSecretValue");
+
+let secretAgent = {
+  name: "Agent Smith",
+  mission: "Classified",
+  [calculateSecretValue]() {
+    return this.name + " - Secret Value: 007";
+  },
+};
+
+console.log(secretAgent.name);
+console.log(secretAgent.mission);
+
+console.log(secretAgent[calculateSecretValue]());
+
+console.log(secretAgent.calculateSecretValue);
