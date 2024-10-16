132907cb7594190e5d5aeecbd5bbd900015ba424
Szymon Zych <szymonzych868@gmail.com>
2024-10-16 23:37:54 +0200

    Add filter examples for user names based on length

--- javascript-practice/practical-js-examples/filter-find.js
+++ javascript-practice/practical-js-examples/filter-find.js
@@ -9,3 +9,14 @@ console.log(olderUser);
 const olderUser1 = users.filter((user) => user.age > 30);
 console.log(olderUser1);
 
+const users1 = [
+  { id: 1, name: "Alice", age: 25 },
+  { id: 2, name: "Bob", age: 30 },
+  { id: 3, name: "Charlie", age: 35 },
+];
+
+const longNameUsers = users1.filter((user) => user.name.length > 3);
+console.log(longNameUsers);
+
+const exactLengthNameUsers = users1.filter((user) => user.name.length === 3);
+console.log(exactLengthNameUsers);
