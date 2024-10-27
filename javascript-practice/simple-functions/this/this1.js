90326936d1801e89a57fbd24abc64615e3acfb44
Szymon Zych <szymonzych868@gmail.com>
2024-10-27 22:12:40 +0100

    add Animal class with displayInfo method and compare static method

--- javascript-practice/simple-functions/this/this1.js
+++ javascript-practice/simple-functions/this/this1.js
@@ -16,3 +16,24 @@ function Car(make, model, year) {
 const myCar = new Car("Ford", "Focus", 2020);
 myCar.displayInfo();
 
+class Animal {
+  constructor(name, species) {
+    this.name = name;
+    this.species = species;
+  }
+
+  displayInfo() {
+    console.log(`Animal: ${this.name} ${this.species}`);
+  }
+  static compare(animall1, animal2) {
+    return animall1.species === animal2.species;
+  }
+}
+
+const dog = new Animal("Rex", "dog");
+const cat = new Animal("Fluffy", "cat");
+
+dog.displayInfo();
+cat.displayInfo();
+
+console.log(Animal.compare(dog, cat));
