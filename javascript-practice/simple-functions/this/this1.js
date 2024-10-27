6e0eefcc75f71340c5950763529fa8a583721ffa
Szymon Zych <szymonzych868@gmail.com>
2024-10-27 22:07:20 +0100

    Refactor code to use unique symbols for object properties and add Car class with displayInfo method

--- javascript-practice/simple-functions/this/this1.js
+++ javascript-practice/simple-functions/this/this1.js
@@ -4,3 +4,15 @@ const person = {
     console.log(`Hello, my name is ${this.name}`);
   },
 };
+
+function Car(make, model, year) {
+  this.make = make;
+  this.model = model;
+  this.year = year;
+  this.displayInfo = function () {
+    console.log(`Car: ${this.year} ${this.make} ${this.model}`);
+  };
+}
+const myCar = new Car("Ford", "Focus", 2020);
+myCar.displayInfo();
+
