86f91af447cdb539bc0cdce6287309da54cbf8d9
Szymon Zych <szymonzych868@gmail.com>
2024-11-26 20:07:38 +0100

    Add allTwoSum function for finding all pairs that sum to target

--- javascript-practice/algorithms/twoSum/twoSum.js
+++ javascript-practice/algorithms/twoSum/twoSum.js
@@ -15,3 +15,22 @@ const nums = [2, 7, 11, 15];
 const target = 9;
 
 console.log(twoSum(nums, target));
+
+//2
+function allTwoSum(nums, target) {
+  const map = new Map();
+  const result = [];
+  for (let i = 0; i < nums.length; i++) {
+    const complement = target - nums[i];
+    if (map.has(complement)) {
+      result.push([map.get(complement), i]);
+    }
+    map.set(nums[i], i);
+  }
+  return result;
+}
+
+const nums1 = [2, 7, 11, 15, 7, 2];
+const target1 = 9;
+
+console.log(allTwoSum(nums1, target1));
