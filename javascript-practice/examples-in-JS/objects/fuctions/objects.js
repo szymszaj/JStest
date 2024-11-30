ed6162094a911715c4559cea7bdf6555e7b875a9
Szymon Zych <szymonzych868@gmail.com>
2024-11-30 22:59:04 +0100

    add Dog constructor and getDog function for creating dog objects

---
+++ javascript-practice/examples-in-JS/objects/fuctions/objects.js
@@ -0,0 +1,30 @@
+function getDog(nane, breed, age, weight) {
+  return {
+    name,
+    breed,
+    age,
+    weight,
+    eat() {
+      console.log(this.name + ": Chomp");
+    },
+    bark() {
+      console.log(this.name + ": Woof");
+    },
+  };
+}
+function Dog(name, breed, age, weight) {
+  this.name = name;
+  this.breed = breed;
+  this.age = age;
+  this.weight = weight;
+  this.eat = function () {
+    console.log(this.name + ": Chomp");
+  };
+  this.bark = function () {
+    console.log(this.name + ": Woof");
+  };
+}
+
+const anotherDog = new Dog("Buddy", "Golden Retriever", 5, 30);
+
+console.log(anotherDog);
