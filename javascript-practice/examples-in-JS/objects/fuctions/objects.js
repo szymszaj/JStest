82334495c6c2e5a3680d6c1c87d2a08ca56dbfbe
Szymon Zych <szymonzych868@gmail.com>
2024-12-09 22:38:00 +0100

    Add methods to manage book borrowing and returning in the library object

--- javascript-practice/examples-in-JS/objects/fuctions/objects.js
+++ javascript-practice/examples-in-JS/objects/fuctions/objects.js
@@ -51,12 +51,60 @@ const target = 9;
 
 console.log(twoSumSolver.findPairs(nums, target));
 
-
 const library = {
   books: [
-    { title: "The Great Gatsby", author: "F. Scott Fitzgerald", available: true },
+    {
+      title: "The Great Gatsby",
+      author: "F. Scott Fitzgerald",
+      available: true,
+    },
     { title: "1984", author: "George Orwell", available: false },
     { title: "To Kill a Mockingbird", author: "Harper Lee", available: true },
-    { title: "The Catcher in the Rye", author: "J.D. Salinger", available: true },
+    {
+      title: "The Catcher in the Rye",
+      author: "J.D. Salinger",
+      available: true,
+    },
     { title: "Moby-Dick", author: "Herman Melville", available: false },
   ],
+
+  listAvailableBooks() {
+    console.log("Available books:");
+    this.books.forEach((book) => {
+      if (book.available) {
+        console.log(`${book.title} by ${book.author}`);
+      }
+    });
+  },
+
+  borrowBook(title) {
+    const book = this.books.find((book) => book.title === title);
+    if (book && book.available) {
+      book.available = false;
+      console.log(`You have borrowed "${book.title}" by ${book.author}`);
+    } else if (book) {
+      console.log(`Sorry, "${book.title}" is currently unavailable.`);
+    } else {
+      console.log(`Sorry, we don't have "${title}" in our library.`);
+    }
+  },
+
+  returnBook(title) {
+    const book = this.books.find((book) => book.title === title);
+    if (book && !book.available) {
+      book.available = true;
+      console.log(`You have returned "${book.title}" by ${book.author}`);
+    } else if (book) {
+      console.log(`"${book.title}" was not borrowed.`);
+    } else {
+      console.log(`Sorry, we don't have "${title}" in our library.`);
+    }
+  },
+};
+// Przykładowe użycie
+library.listAvailableBooks();
+library.borrowBook("1984");
+library.borrowBook("The Great Gatsby");
+library.listAvailableBooks();
+library.returnBook("The Great Gatsby");
+library.listAvailableBooks();
