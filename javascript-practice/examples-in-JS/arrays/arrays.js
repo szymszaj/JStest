9b39f1df2f057b84983b3a8aa2cd59ec77b97553
Szymon Zych <szymonzych868@gmail.com>
2024-12-26 22:20:20 +0100

    Update employee salary calculation in arrays.js to reflect a 70% increase

--- javascript-practice/examples-in-JS/arrays/arrays.js
+++ javascript-practice/examples-in-JS/arrays/arrays.js
@@ -1,3 +1,38 @@
-const characters = ["Homer", "Marge", "Bart", "Lisa", "Maggie"];
-const upperCaseCharacters = characters.map((char) => char.toUpperCase());
-console.log(upperCaseCharacters)
+const employees = [
+  {
+    id: 1,
+    name: "John",
+    department: "HR",
+    salary: 1000,
+  },
+  {
+    id: 2,
+    name: "Jane",
+    department: "IT",
+    salary: 1500,
+  },
+  {
+    id: 3,
+    name: "Alice",
+    department: "HR",
+    salary: 1200,
+  },
+  {
+    id: 4,
+    name: "Bob",
+    department: "IT",
+    salary: 1300,
+  },
+  {
+    id: 5,
+    name: "Eve",
+    department: "HR",
+    salary: 1100,
+  },
+];
+
+const updatedEmployees = employees.map((employee) => ({
+  ...employee,
+  salary: employee.salary * 1.7,
+}));
+console.log(updatedEmployees);
