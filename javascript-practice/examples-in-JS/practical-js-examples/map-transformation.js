f6f26b441f5ea7c59f589c73892355d869794f53
Szymon Zych <szymonzych868@gmail.com>
2025-01-08 21:43:26 +0100

    Enhance map-transformation.js to include user salary mapping and grouping by job functionality

--- javascript-practice/examples-in-JS/practical-js-examples/map-transformation.js
+++ javascript-practice/examples-in-JS/practical-js-examples/map-transformation.js
@@ -5,12 +5,14 @@ const exampleUser = [
   { id: 21, name: "David", age: 40, job: "developer" },
   { id: 21, name: "David", age: 40 },
 ];
+
 const userNames = exampleUser.map((user) => ({
   userId: user.id,
   userName: user.name,
   useAge: user.age,
   userJob: user.job || "No job",
 }));
+console.log("User Names:");
 console.log(userNames);
 
 const User = [
@@ -20,8 +22,30 @@ const User = [
   { salary: 4000, name: "David" },
   { salary: 5000, name: "Eve" },
 ];
+
 const userSalary = User.map((user) => ({
   userName: user.name,
   userSalary: user.salary,
 }));
+console.log("User Salaries:");
 console.log(userSalary);
+
+function groupUsersByJob(users) {
+  return users.reduce((acc, user) => {
+    const job = user.job || "No job";
+    if (!acc[job]) {
+      acc[job] = [];
+    }
+    acc[job].push(user);
+    return acc;
+  }, {});
+}
+
+const updatedUsers = updateUser(exampleUser, 55, {
+  age: 26,
+  job: "senior developer",
+});
+const groupedByJob = groupUsersByJob(updatedUsers);
+
+console.log("Grouped by Job:");
+console.log(groupedByJob);
