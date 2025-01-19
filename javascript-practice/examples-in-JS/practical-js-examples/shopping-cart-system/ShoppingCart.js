b93c72004c80160b7bb011fef47608f142a6f5dc
Szymon Zych <szymonzych868@gmail.com>
2025-01-19 22:38:10 +0100

    Add sample items and discount to ShoppingCart for demonstration

--- javascript-practice/examples-in-JS/practical-js-examples/shopping-cart-system/ShoppingCart.js
+++ javascript-practice/examples-in-JS/practical-js-examples/shopping-cart-system/ShoppingCart.js
@@ -53,3 +53,23 @@ class ShoppingCart {
     };
   }
 }
+
+const cart = new ShoppingCart();
+
+cart.addItem({
+  id: 66,
+  name: "Boss",
+  price: 209.99,
+  quantity: 2,
+});
+
+cart.addItem({
+  id: 2,
+  name: "Hugo Boss",
+  price: 49.99,
+  quantity: 1,
+});
+
+cart.addDiscount("SUMMER20", 20);
+
+console.log(cart.getCartSummary());
