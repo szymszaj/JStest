318f80598bc54ce1da4801f6df28a0de2955b348
Szymon Zych <szymonzych868@gmail.com>
2025-01-25 22:26:22 +0100

    Add sections data module with structured titles and links

---
+++ javascript-practice/sectionsRenderer/data/sections.js
@@ -0,0 +1,30 @@
+export const sections = [
+  {
+    title: "Wstęp",
+    links: [
+      { text: "Link 1", href: "https://www.google.pl/?hl=pl" },
+      { text: "Link 2", href: "https://www.google.pl/?hl=pl" },
+    ],
+  },
+  {
+    title: "Rozdział 1",
+    links: [
+      { text: "Link 3", href: "https://www.google.pl/?hl=pl" },
+      { text: "Link 4", href: "https://www.google.pl/?hl=pl" },
+    ],
+  },
+  {
+    title: "Rozdział 2",
+    links: [
+      { text: "Link 5", href: "https://www.google.pl/?hl=pl" },
+      { text: "Link 6", href: "https://www.google.pl/?hl=pl" },
+    ],
+  },
+  {
+    title: "Rozdział 3",
+    links: [
+      { text: "Link 7", href: "https://www.google.pl/?hl=pl" },
+      { text: "Link 8", href: "https://www.google.pl/?hl=pl" },
+    ],
+  },
+];
