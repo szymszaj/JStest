7dc236ef032e81794302833241b5758ab5079a25
Szymon Zych <szymonzych868@gmail.com>
2025-02-03 23:41:55 +0100

    Add async function to fetch multiple data sources concurrently

--- javascript-practice/examples-in-JS/objects/promes/indexPromes.js
+++ javascript-practice/examples-in-JS/objects/promes/indexPromes.js
@@ -25,3 +25,27 @@ fetchData()
   .catch((error) => {
     console.error(error);
   });
+
+function fetchData1() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve("Data 1 fetched!");
+    }, 2000);
+  });
+}
+
+function fetchData2() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve("Data 2 fetched!");
+    }, 3000);
+  });
+}
+
+async function fetchAllData() {
+  let [data1, data2] = await Promise.all([fetchData1(), fetchData2()]);
+  console.log(data1);
+  console.log(data2);
+}
+
+fetchAllData();
