3662136da00e0d78b695c86948365ad42a15d0b0
Szymon Zych <szymonzych868@gmail.com>
2025-02-08 23:39:21 +0100

    Add example demonstrating filter, map, and reduce methods for array manipulation

--- javascript-practice/examples-in-JS/simple-functions/map/index.js
+++ javascript-practice/examples-in-JS/simple-functions/map/index.js
@@ -6,3 +6,9 @@ const numbers1 = [10, 20, 30, 40];
 const filtered = numbers1.filter((num) => num >= 30);
 console.log(filtered);
 
+const numbersMap = [1, 2, 3, 4, 5, 6];
+const result = numbersMap
+  .filter((num) => num % 2 === 0)
+  .map((num) => num ** 10)
+  .reduce((acc, num) => acc + num, 0);
+console.log(result);
