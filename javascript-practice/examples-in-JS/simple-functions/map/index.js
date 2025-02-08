2b15875b1505d378927bc6414a6fbdf38457d951
Szymon Zych <szymonzych868@gmail.com>
2025-02-08 23:37:07 +0100

    Add filtering example using filter method to demonstrate array manipulation

--- javascript-practice/examples-in-JS/simple-functions/map/index.js
+++ javascript-practice/examples-in-JS/simple-functions/map/index.js
@@ -2,3 +2,7 @@ const numbers = [1, 2, 3, 4, 5];
 const squared = numbers.map((num) => num ** 4);
 console.log(squared);
 
+const numbers1 = [10, 20, 30, 40];
+const filtered = numbers1.filter((num) => num >= 30);
+console.log(filtered);
+
