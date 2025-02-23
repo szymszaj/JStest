0e875f12996dc5671cf80c3a9640de7acda04b8d
Szymon Zych <szymonzych868@gmail.com>
2025-02-23 22:59:17 +0100

    Add functions to sort and display fruits by price and filter products by minimum price

---
+++ javascript-practice/examples-in-JS/simple-functions/functions-1/app.js
@@ -0,0 +1,40 @@
+const product = [
+  { name: "Apple", price: 3 },
+  { name: "Banana", price: 1 },
+  { name: "Cherry", price: 2 },
+];
+
+function sortAndDisplayFruits(order) {
+  if (order === "most expensive") {
+    product.sort((a, b) => b.price - a.price);
+    console.log("Most expensive fruits:");
+  } else if (order === "cheapest") {
+    product.sort((a, b) => a.price - b.price);
+    console.log("Cheapest fruits:");
+  } else {
+    console.log("Invalid command. Choose 'most expensive' or 'cheapest'.");
+    return;
+  }
+  product.forEach((fruit) => {
+    console.log(`${fruit.name}: $${fruit.price}`);
+  });
+}
+
+const products = [
+  { name: "Laptop", price: 1000 },
+  { name: "Phone", price: 500 },
+  { name: "Tablet", price: 700 },
+  { name: "Monitor", price: 300 },
+];
+
+function filterAndSortProducts(products, minPrice) {
+  const filteredProducts = products.filter(
+    (product) => product.price >= minPrice
+  );
+
+  filteredProducts.sort((a, b) => a.price - b.price);
+
+  return filteredProducts;
+}
+
+console.log(filterAndSortProducts(products, 600));
