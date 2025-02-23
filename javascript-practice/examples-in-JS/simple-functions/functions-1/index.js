07718ca3ad10da495986a361bd58fcb422db4a2e
Szymon Zych <szymonzych868@gmail.com>
2025-02-23 22:57:51 +0100

    Add function to sort and display fruits by price

--- javascript-practice/examples-in-JS/simple-functions/functions-1/index.js
+++ javascript-practice/examples-in-JS/simple-functions/functions-1/index.js
@@ -13,3 +13,25 @@ function isEvenOrOdd(number) {
 
 console.log(isEvenOrOdd(4));
 console.log(isEvenOrOdd(7));
+
+const product = [
+  { name: "Apple", price: 3 },
+  { name: "Banana", price: 1 },
+  { name: "Cherry", price: 2 },
+];
+
+function sortAndDisplayFruits(order) {
+  if (order === "most expensive") {
+    product.sort((a, b) => b.price - a.price);
+    console.log("Most expensive fruits:");
+  } else if (order === "cheapest") {
+    product.sort((a, b) => a.price - b.price);
+    console.log("Cheapest fruits:");
+  } else {
+    console.log("Invalid command. Choose 'most expensive' or 'cheapest'.");
+    return;
+  }
+  product.forEach((fruit) => {
+    console.log(`${fruit.name}: $${fruit.price}`);
+  });
+}
