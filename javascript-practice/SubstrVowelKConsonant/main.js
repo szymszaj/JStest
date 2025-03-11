2c34f819d144ef009a6797a86504903b21a3746a
Szymon Zych <szymonzych868@gmail.com>
2025-03-11 23:11:04 +0100

    Add main.js to initialize SubstrVowelKConsonant class and demonstrate its methods

---
+++ javascript-practice/SubstrVowelKConsonant/main.js
@@ -0,0 +1,9 @@
+import SubstrVowelKConsonant from "./SubstrVowelKConsonant.js";
+import "./countOfSubstrings.js";
+import "./countVowelsAndConsonants.js";
+import "./longestSubstringWithAllVowels.js";
+
+const instance = new SubstrVowelKConsonant();
+console.log(instance.countOfSubstrings("yourwordhere", 2));
+console.log(instance.countVowelsAndConsonants("yourwordhere"));
+console.log(instance.longestSubstringWithAllVowels("yourwordhere"));
