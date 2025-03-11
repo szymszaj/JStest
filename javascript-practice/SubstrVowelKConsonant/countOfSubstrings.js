261f6d2c52c393be4d6baa70415bcef3c79045ec
Szymon Zych <szymonzych868@gmail.com>
2025-03-11 23:10:51 +0100

    Add countOfSubstrings method to SubstrVowelKConsonant class for counting valid substrings

---
+++ javascript-practice/SubstrVowelKConsonant/countOfSubstrings.js
@@ -0,0 +1,43 @@
+import SubstrVowelKConsonant from "./SubstrVowelKConsonant.js";
+
+SubstrVowelKConsonant.prototype.countOfSubstrings = function (word, k) {
+  let n = word.length;
+  return this.atleastK(word, k, n) - this.atleastK(word, k + 1, n);
+};
+
+SubstrVowelKConsonant.prototype.atleastK = function (word, minKReq, n) {
+  let vowelFreq = new Map();
+  let lt = 0,
+    rt = -1,
+    nonVowel = 0;
+  let result = 0;
+
+  while (++rt < n) {
+    if (SubstrVowelKConsonant.vowel.includes(word[rt])) {
+      if (vowelFreq.has(word[rt])) {
+        vowelFreq.set(word[rt], vowelFreq.get(word[rt]) + 1);
+      } else {
+        vowelFreq.set(word[rt], 1);
+      }
+    } else {
+      nonVowel++;
+    }
+
+    while (nonVowel >= minKReq && vowelFreq.size === 5) {
+      result += n - rt;
+      if (SubstrVowelKConsonant.vowel.includes(word[lt])) {
+        if (vowelFreq.get(word[lt]) - 1 === 0) {
+          vowelFreq.delete(word[lt]);
+        } else {
+          vowelFreq.set(word[lt], vowelFreq.get(word[lt]) - 1);
+        }
+      } else {
+        nonVowel--;
+      }
+
+      lt++;
+    }
+  }
+
+  return result;
+};
