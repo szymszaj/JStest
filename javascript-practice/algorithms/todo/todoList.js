5958b3a1a8f35db9b52e5dfe5acda0bb92ff7e18
Szymon Zych <szymonzych868@gmail.com>
2025-03-30 22:18:54 +0200

    Add task management functions to todoList module

--- javascript-practice/algorithms/todo/todoList.js
+++ javascript-practice/algorithms/todo/todoList.js
@@ -2,5 +2,34 @@ let tasks = [];
 
 function addTask(task) {
   tasks.push(task);
-  console.log(`Mamy zadanie: ${task}`);
+  console.log(`Dodano zadanie: "${task}"`);
 }
+
+function removeTask(taskIndex) {
+  if (taskIndex >= 0 && taskIndex < tasks.length) {
+    const removedTask = tasks.splice(taskIndex, 1);
+    console.log(`Usunięto zadanie: "${removedTask}"`);
+  } else {
+    console.log("Niepoprawny indeks zadania.");
+  }
+}
+
+function showTasks() {
+  if (tasks.length === 0) {
+    console.log("Brak zadań do wyświetlenia.");
+  } else {
+    console.log("Lista zadań:");
+    tasks.forEach((task, index) => {
+      console.log(`${index + 1}. ${task}`);
+    });
+  }
+}
+
+addTask("Napisać aplikację");
+addTask("Przetestować kod");
+
+showTasks();
+
+removeTask(1);
+
+showTasks();
