25516bd34f241fa5bbf1ae2aef064af55589a05a
Szymon Zych <szymonzych868@gmail.com>
2025-03-31 23:40:52 +0200

    Add examples of array spreading and arrow functions in bin.js

--- javascript-practice/examples-in-JS/bin_/bin.js
+++ javascript-practice/examples-in-JS/bin_/bin.js
@@ -1,3 +1,13 @@
 const person = { name: "John", age: 30, city: "New York" };
 const { name, age } = person;
 console.log(name, age);
+
+const arr = [1, 2, 3, 4, 5];
+const newArr = [...arr, 6, 7, 8];
+console.log(newArr);
+
+const exampleBin = {a,b} => a+b
+console.log(add(2,5))
+
+console.log(exampleBin)
+
