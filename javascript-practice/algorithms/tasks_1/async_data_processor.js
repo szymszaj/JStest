0b3aa76de32fad101dec67b483508c3aebe4b956
Szymon Zych <szymonzych868@gmail.com>
2025-04-03 22:17:57 +0200

    Add notify method and initial fetchData implementation to DataProcessor class

--- javascript-practice/algorithms/tasks_1/async_data_processor.js
+++ javascript-practice/algorithms/tasks_1/async_data_processor.js
@@ -25,4 +25,15 @@ class DataProcessor {
   unsubscribe(observer) {
     this.observer = this.observers.filter((obs) => obs !== observer);
   }
+  notify(data) {
+    this.observers.forEach((observer) => observer.update(data));
+  }
+
+  async ferchData(url, retries = 3, baseDelay = 300) {
+    if (this.cache.has(url)) {
+      console.log(`${new Date().toISOString()} - Cache hit for ${url}`);
+      return this.cache.get(url);
+    }
+    let attempt = 0;
+  }
 }
