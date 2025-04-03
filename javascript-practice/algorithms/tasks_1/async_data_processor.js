370d56c4175783827a5be6ff2298fc9041f895e6
Szymon Zych <szymonzych868@gmail.com>
2025-04-03 22:15:27 +0200

    Add observer pattern methods to DataProcessor class for better event handling

--- javascript-practice/algorithms/tasks_1/async_data_processor.js
+++ javascript-practice/algorithms/tasks_1/async_data_processor.js
@@ -13,4 +13,16 @@ class DataProcessor {
 
     DataProcessor.instance = this;
   }
+
+  subscribe(observer) {
+    if (typeof observer.update === "function") {
+      this.observers.push(observer);
+      return () => this.unsubscribe(observer);
+    }
+    throw new Error("Observer must implement update method");
+  }
+
+  unsubscribe(observer) {
+    this.observer = this.observers.filter((obs) => obs !== observer);
+  }
 }
