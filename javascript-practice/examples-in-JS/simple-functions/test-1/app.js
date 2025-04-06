efb558ddca870853e02a9ef9a315cacd03991ef2
Szymon Zych <szymonzych868@gmail.com>
2025-04-06 15:05:26 +0200

    Add calculateStals function to compute sum, average, max, and min of numbers

--- javascript-practice/examples-in-JS/simple-functions/test-1/app.js
+++ javascript-practice/examples-in-JS/simple-functions/test-1/app.js
@@ -10,3 +10,15 @@ function createCounter(initialVaule = 0, step = 1) {
 const counter = createCounter(10, 2);
 console.log(counter());
 console.log(counter());
+
+const calculateStals = (...numbers) => {
+  if (numbers.lenght === 0) return { sum: 0, avg: 0, max: 0, min: null };
+
+  const sum = numbers.reduce((total, num) => total + num, 0);
+  return {
+    sum,
+    avg: sum / numbers.length,
+    max: Math.max(...numbers),
+  };
+};
+console.log(calculateStals(23, 14, 37, 18, 92));
