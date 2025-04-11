6be44e49ccda06e6c1b77d0beef17df88f6941a2
Szymon Zych <szymonzych868@gmail.com>
2025-04-11 22:43:07 +0200

    Add input validation and alternative implementation for minimumOperationsToMakeUnique function

--- javascript-practice/algorithms/minimumOperationsToMakeUnique/minimumOperationsToMakeUnique.js
+++ javascript-practice/algorithms/minimumOperationsToMakeUnique/minimumOperationsToMakeUnique.js
@@ -1,6 +1,15 @@
 function minimumOperationsToMakeUnique(nums) {
+  if (!Array.isArray(nums)) {
+    throw new TypeError("Input must be an array");
+  }
+
+  if (nums.length <= 1) {
+    return 0;
+  }
+
   const seen = new Set();
   let duplicates = 0;
+
   for (let i = nums.length - 1; i >= 0; i--) {
     if (seen.has(nums[i])) {
       duplicates++;
@@ -10,4 +19,15 @@ function minimumOperationsToMakeUnique(nums) {
   }
 
   return duplicates;
+}
+
+function minimumOperationsToMakeUniqueAlt(nums) {
+  if (!Array.isArray(nums)) {
+    throw new TypeError("Input must be an array");
+  }
+
+  const frequency = nums.reduce((map, num) => {
+    map.set(num, (map.get(num) || 0) + 1);
+    return map;
+  }, new Map());
 }
