7239dec8be653c58ce05c805353f28bea41c36f9
Szymon Zych <szymonzych868@gmail.com>
2025-04-13 20:40:31 +0200

    Fix console log syntax, improve for loop formatting, and add exotic fruits list and person object with greet method

--- javascript-practice/examples-in-JS/js-practice/practice.js
+++ javascript-practice/examples-in-JS/js-practice/practice.js
@@ -2,9 +2,26 @@ function calculateSum(a, b) {
   return a + b;
 }
 
-console.log("suma 2  i3 to:" calculateSum(2, 3));
+console.log("suma 2  i3 to:", calculateSum(2, 3));
 
 console.log("for");
-for (let i = 1; i<=5; i++) {
+for (let i = 1; i <= 5; i++) {
   console.log(i);
 }
+
+const exoticFruis = ["banana", "mango", "kiwi"];
+console.log("Lista owoców");
+exoticFruis.forEach((fruit, index) => {
+  console.log(`Owoc ${index + 1}: ${fruit}`);
+});
+
+const personInJob - {
+  name: "John",
+  age: 30,
+  job: "developer",
+  greet: function() {
+    console.log(`Cześć, jestem ${this.name} i mam ${this.age} lat.`);
+  },
+}
+
+personInJob.greet();
