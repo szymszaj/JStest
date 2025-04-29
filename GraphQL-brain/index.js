7af2437ebd0412f2c07a236a302cef22a724cfee
Szymon Zych <szymonzych868@gmail.com>
2025-04-29 22:25:59 +0200

    Set up Apollo Server with type definitions and resolvers for hello query

--- GraphQL-brain/index.js
+++ GraphQL-brain/index.js
@@ -1,7 +1,18 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
 
-const server = new ApolloServer({});
+const server = new ApolloServer({
+  typeDefs: `#graphql
+    type Query {
+      hello: String
+    }
+  `,
+  resolvers: {
+    Query: {
+      hello: () => "Hello world!",
+    },
+  },
+});
 
 const { url } = await startStandaloneServer(server, {
   listen: { port: 4000 },
