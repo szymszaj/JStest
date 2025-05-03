876038294493e58a3e70d29ab13a5239173231b0
Szymon Zych <szymonzych868@gmail.com>
2025-05-03 19:39:05 +0200

    feat: integrate dotenv for environment variable management
    - Added dotenv package to manage environment variables securely.
    - Created config files for dotenv to handle CLI and environment options.
    - Implemented dotenv configuration in firebase-config.js to load Firebase credentials from environment variables.
    - Updated package.json and package-lock.json to include dotenv as a dependency.
    - Added TypeScript definitions for dotenv to improve type safety.

--- chat-app/src/firebase-config.js
+++ chat-app/src/firebase-config.js
@@ -1,17 +1,21 @@
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
+import { getAnalytics } from "firebase/analytics";
+import dotenv from "dotenv";
 
-//TO DO: replace with Firebase project configuration
+dotenv.config();
+
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: "",
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-export const db = getFirestore(app);
+const analytics = getAnalytics(app);
+
+export default app;
