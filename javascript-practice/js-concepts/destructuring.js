7c578f33d144adca5a3a3adb96110e78cc6eaf58
Szymon Zych <szymonzych868@gmail.com>
2025-05-11 16:24:32 +0200

    feat: add destructuring example with person and numbers

---
+++ javascript-practice/js-concepts/destructuring.js
@@ -0,0 +1,9 @@
+const person = { name: "Jon", age: 30, city: "Warszawa" };
+const { name, age } = person;
+
+console.log(name);
+console.log(age);
+
+const numbers = [1, 2, 3, 4, 5];
+const [first, serverd, third] = numbers;
+console.log(first);
