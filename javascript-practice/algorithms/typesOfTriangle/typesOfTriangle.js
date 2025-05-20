3f6320b76bc1f60adaf8bdcb62e3919f68d489e4
Szymon Zych <szymonzych868@gmail.com>
2025-05-20 22:12:46 +0200

    feat: implement typeOfTriangle function to classify triangles based on side lengths

---
+++ javascript-practice/algorithms/typesOfTriangle/typesOfTriangle.js
@@ -0,0 +1,19 @@
+function typeOfTriangle(nums) {
+  if (
+    nums[0] + nums[1] <= nums[2] ||
+    nums[2] + nums[1] <= nums[0] ||
+    nums[2] + nums[0] <= nums[1]
+  ) {
+    return "none";
+  }
+
+  if (nums[0] === nums[1] && nums[2] === nums[1]) {
+    return "equilateral";
+  }
+
+  if (nums[0] === nums[1] || nums[2] === nums[1] || nums[2] === nums[0]) {
+    return "isosceles";
+  }
+
+  return "scalene";
+}
