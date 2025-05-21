d4512428b180ceae29a27b24dd015eb417fbe2b8
Szymon Zych <szymonzych868@gmail.com>
2025-05-21 21:19:40 +0200

    feat: add permutations function to generate all possible arrangements of an array

--- javascript-practice/examples-in-JS/arrow-functions/arrow-functions.js
+++ javascript-practice/examples-in-JS/arrow-functions/arrow-functions.js
@@ -1,3 +1,22 @@
 const adultNames = (people) =>
   people.filter((person) => person.age > 18).map((person) => person.name);
 
+const permutations = (arr) =>
+  arr.length === 0
+    ? [[]]
+    : arr.flatMap((v, i) =>
+        permutations([...arr.slice(0, i), ...arr.slice(i + 1)]).map((p) => [
+          v,
+          ...p,
+        ])
+      );
+
+console.log(
+  adultNames([
+    { name: "Adam", age: 15 },
+    { name: "Ania", age: 22 },
+    { name: "Jan", age: 30 },
+  ])
+);
+
+console.log(permutations([1, 2, 3]));
