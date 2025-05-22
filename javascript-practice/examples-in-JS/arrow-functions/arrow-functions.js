6ac552db4b7f66e6e10b076470727b3da7b9a4b2
Szymon Zych <szymonzych868@gmail.com>
2025-05-22 21:09:50 +0200

    feat: add adultNames1 function and generator-based permutations function

--- javascript-practice/examples-in-JS/arrow-functions/arrow-functions.js
+++ javascript-practice/examples-in-JS/arrow-functions/arrow-functions.js
@@ -20,3 +20,36 @@ console.log(
 );
 
 console.log(permutations([1, 2, 3]));
+
+const adultNames1 = (people) =>
+  Array.isArray(people)
+    ? people.reduce((acc, person) => {
+        if ((person.age ?? 0) > 18 && person.name) acc.push(person.name);
+        return acc;
+      }, [])
+    : [];
+
+function* permutations(arr, n = arr.length) {
+  if (n <= 1) {
+    yield arr.slice();
+  } else {
+    for (let i = 0; i < n; i++) {
+      yield* permutations(arr, n - 1);
+      if (n % 2 === 0) {
+        [arr[i], arr[n - 1]] = [arr[n - 1], arr[i]];
+      } else {
+        [arr[0], arr[n - 1]] = [arr[n - 1], arr[0]];
+      }
+    }
+  }
+}
+
+console.log(
+  adultNames([
+    { name: "Adam", age: 15 },
+    { name: "Ania", age: 22 },
+    { name: "Jan", age: 30 },
+  ])
+);
+
+console.log([...permutations([1, 2, 3])]);
