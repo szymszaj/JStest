6f96680533cd4e989c9e85a5e334ffa4afdddae8
Szymon Zych <szymonzych868@gmail.com>
2025-06-10 21:23:36 +0200

    feat: add Person class with methods for age calculation and hobby management

---
+++ aboutMe/aboutMe.js
@@ -0,0 +1,50 @@
+class Person {
+  constructor(
+    firstName,
+    lastName,
+    birthDate,
+    country = "Poland",
+    hobbies = []
+  ) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.birthDate = new Date(birthDate);
+    this.country = country;
+    this.hobbies = hobbies;
+  }
+  getAge() {
+    const today = new Date();
+    let age = today.getFullYear() - this.birthDate.getFullYear();
+    const hasBirthdayPassed =
+      today.getMonth() > this.birthDate.getMonth() ||
+      (today.getMonth() === this.birthDate.getMonth() &&
+        today.getDate() >= this.birthDate.getDate());
+    if (!hasBirthdayPassed) {
+      age--;
+    }
+    return age;
+  }
+
+  describe() {
+    console.log(
+      `Hi, my name is ${this.firstName} ${
+        this.lastName
+      }. I am ${this.getAge()} years old and I live in ${this.country}.`
+    );
+    if (this.hobbies.length) {
+      console.log("My hobbies include:");
+      this.hobbies.forEach((hobby) => console.log(`- ${hobby}`));
+    } else {
+      console.log("I currently have no hobbies.");
+    }
+  }
+
+  addHobby(newHobby) {
+    if (!this.hobbies.includes(newHobby)) {
+      this.hobbies.push(newHobby);
+      console.log(`Added new hobby: ${newHobby}`);
+    } else {
+      console.log(`Hobby "${newHobby}" already exists!`);
+    }
+  }
+}
