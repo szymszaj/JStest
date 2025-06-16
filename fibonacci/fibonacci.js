9ea90c7dff47c55b7edc95f285b7cb1a0f6378f8
Szymon Zych <szymonzych868@gmail.com>
2025-06-16 22:23:23 +0200

    feat: add iterative version of Fibonacci function without using an array

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -29,3 +29,15 @@ function fibonacciRecursive(n) {
 }
 
 console.log(fibonacciRecursive(5));
+
+// Iterative version without using an array
+function fibonacciNth(n) {
+  if (n <= 0) return 0;
+  let a = 0,
+    b = 1;
+  for (let i = 2; i <= n; i++) {
+    [a, b] = [b, a + b];
+  }
+  return n === 1 ? a : b;
+}
+console.log(fibonacciNth(6));
