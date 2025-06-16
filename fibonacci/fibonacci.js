09e522c40edc7be4afc4d2bf02c1a2acce256e25
Szymon Zych <szymonzych868@gmail.com>
2025-06-16 22:17:11 +0200

    feat: add fibonacci_2 function to return the n-th Fibonacci element and a recursive version

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -7,3 +7,25 @@ function fibonacci(n) {
   }
   return seq;
 }
+
+// Ability to return only the n-th element
+function fibonacci_2(n, onlyLast = flase) {
+  if (n <= 0) return onlyLast ? null : [];
+  if (n === 1) return onlyLast ? 0 : [0];
+  const seq = [0, 1];
+  for (let i = 2; i < n; i++) {
+    seq.push(seq[i - 1] + seq[i - 2]);
+  }
+  return onlyLast ? seq[n - 1] : seq;
+}
+
+console.log(fibonacci_2(10));
+console.log(fibonacci_2(10, true));
+
+// Recursive version (for the n-th element)
+function fibonacciRecursive(n) {
+  if (n <= 1) return;
+  return fibonacciRecursive(n - 1) + fibonacciRecursive(n - 2);
+}
+
+console.log(fibonacciRecursive(5));
