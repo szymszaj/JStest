922c5c7dd6e20171c3db93b3f588804f5ffb501a
Szymon Zych <szymonzych868@gmail.com>
2025-06-25 22:42:21 +0200

    feat: enhance bubbleSort function to support custom sorting options and step logging

--- javascript-practice/algorithms/bubbleSort/bubbleSort.js
+++ javascript-practice/algorithms/bubbleSort/bubbleSort.js
@@ -1,16 +1,21 @@
-function bubbleSort(arr) {
-  let n = arr.length;
+function bubbleSort(arr, options = {}) {
+  const { descending = false, showSteps = false, compareFn = null } = options;
+
+  let sortedArr = arr.slice();
+  let n = sortedArr.length;
+
+  const defaultCompare = (a, b) => (descending ? b - a : a - b);
+  const cmp = compareFn || defaultCompare;
+
   for (let i = 0; i < n - 1; i++) {
     for (let j = 0; j < n - i - 1; j++) {
-      if (arr[j] > arr[j + 1]) {
-        let temp = arr[j];
-        arr[j] = arr[j + 1];
-        arr[j + 1] = temp;
+      if (cmp(sortedArr[j], sortedArr[j + 1]) > 0) {
+        [sortedArr[j], sortedArr[j + 1]] = [sortedArr[j + 1], sortedArr[j]];
+        if (showSteps) {
+          console.log(`Step: ${sortedArr.join(", ")}`);
+        }
       }
     }
   }
-  return arr;
+  return sortedArr;
 }
-
-const numbers = [5, 2, 9, 1, 5, 6];
-console.log(bubbleSort(numbers));
