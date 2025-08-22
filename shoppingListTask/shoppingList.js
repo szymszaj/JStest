70f34596f0091708b6e03503364bdb92b2718b47
Szymon Zych <szymonzych868@gmail.com>
2025-08-22 14:54:09 +0200

    feat: add initial shopping list data

---
+++ shoppingListTask/shoppingList.js
@@ -0,0 +1,11 @@
+const shoppingList = [
+  { name: "chleb", category: "pieczywo", price: 5.5, bought: false },
+  { name: "mleko", category: "nabiał", price: 4.2, bought: true },
+  { name: "masło", category: "nabiał", price: 8.0, bought: false },
+  { name: "szynka", category: "wędliny", price: 18.0, bought: false },
+  { name: "ser", category: "nabiał", price: 14.0, bought: true },
+  { name: "bułki", category: "pieczywo", price: 1.2, bought: false },
+  { name: "pomidor", category: "warzywa", price: 3.3, bought: false },
+];
+
+module.exports = shoppingList;
