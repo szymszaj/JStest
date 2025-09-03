5d6438f8a11ae5e5966fd811255dc22dbdc9901d
Szymon Zych <szymonzych868@gmail.com>
2025-09-03 15:55:24 +0200

    feat: add string utility functions for capitalization, reversal, palindrome check, and word counting

---
+++ test/index.js
@@ -0,0 +1,7 @@
+const math = require("./mathUtils");
+const string = require("./stringUtils");
+const array = require("./arrayUtils");
+
+console.log(math.getAverage([1, 2, 3, 4, 5]));
+console.log(string.isPalindrome("Kajak"));
+console.log(array.unique([1, 1, 2, 3]));
