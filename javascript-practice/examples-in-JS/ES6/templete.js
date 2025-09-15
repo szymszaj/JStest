99af3f52c93e222734633b9f34632ac8095d2181
Szymon Zych <szymonzych868@gmail.com>
2025-09-15 13:43:55 +0200

    feat: add template script for user introduction and role display

---
+++ javascript-practice/examples-in-JS/ES6/templete.js
@@ -0,0 +1,9 @@
+const name = "ola";
+const age = 20;
+console.log(`Hi my name is ${name} and I am ${age} years old`);
+
+const user = { id: 1, name1: "John", role: "admin" };
+const { name1, role } = user;
+
+console.log(`Hi my name is ${name1} and I am ${age} years old`);
+console.log(`My role is ${role}`);
