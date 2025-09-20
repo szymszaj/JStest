b1c2c69a479a52e0e717cfe01d895b609be68827
Szymon Zych <szymonzych868@gmail.com>
2025-09-20 22:10:19 +0200

    feat: add index and test files for Fibonacci functions

---
+++ fibonacci/index.js
@@ -0,0 +1,11 @@
+const fibonacciArray = require("./fibonacciArray");
+const fibonacciNth = require("./fibonacciNth");
+const fibonacciRecursive = require("./fibonacciRecursive");
+const fibonacciWithErrorHandling = require("./fibonacciWithErrorHandling");
+
+module.exports = {
+  fibonacciArray,
+  fibonacciNth,
+  fibonacciRecursive,
+  fibonacciWithErrorHandling,
+};
---
+++ fibonacci/test.js
@@ -0,0 +1,11 @@
+const {
+  fibonacciArray,
+  fibonacciNth,
+  fibonacciRecursive,
+  fibonacciWithErrorHandling,
+} = require("./index");
+
+console.log(fibonacciArray(6));
+console.log(fibonacciNth(6));
+console.log(fibonacciRecursive(6));
+console.log(fibonacciWithErrorHandling(6));
