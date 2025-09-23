399a707ac883ad7b88e124d989da0f5a727dcfbc
Szymon Zych <szymonzych868@gmail.com>
2025-09-23 22:18:21 +0200

    feat: refine GraphQL schema by uncommenting fields in Game, Review, and Author types

--- GraphQL-brain/schema.js
+++ GraphQL-brain/schema.js
@@ -1,25 +1,33 @@
 export const typeDefs = `#graphql
   type Game {
-  id: ID!
-  name: String!
-  # description: String!
-  # releaseDate: String!
-  platforms: [String!]!
+    id: ID!
+    name: String!
+    description: String
+    releaseDate: String
+    platforms: [String!]!
   }
   type Review {
-  id: ID!
-  rating: Int!
-  comment: String!
+    id: ID!
+    rating: Int!
+    comment: String!
+    game: Game!
+    author: Author!
   }
   type Author {
-  id: ID!
-  name: String!
-  verified: Boolean!
+    id: ID!
+    name: String!
+    verified: Boolean!
+    reviews: [Review!]
   }
-  
   type Query {
     reviews: [Review!]
     games: [Game!]
     authors: [Author!]
+    game(id: ID!): Game
+    author(id: ID!): Author
+  }
+  type Mutation {
+    addGame(name: String!, description: String, releaseDate: String, platforms: [String!]!): Game!
+    addReview(rating: Int!, comment: String!, gameId: ID!, authorId: ID!): Review!
   }
 `;
