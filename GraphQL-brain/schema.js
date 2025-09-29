c1b147976852d2f736042009551b52b6e6454d2f
Szymon Zych <szymonzych868@gmail.com>
2025-09-29 21:44:09 +0200

    feat: enhance GraphQL schema by adding fields for Publisher and updating Game, Review, and Author types

--- GraphQL-brain/schema.js
+++ GraphQL-brain/schema.js
@@ -1,10 +1,20 @@
 export const typeDefs = `#graphql
+  type Publisher {
+    id: ID!
+    name: String!
+    country: String
+    founded: String
+    games: [Game!]
+  }
+
   type Game {
     id: ID!
     name: String!
     description: String
     releaseDate: String
     platforms: [String!]!
+    genre: String
+    publisher: Publisher
     reviews: [Review!]
     authors: [Author!]
   }
@@ -13,6 +23,7 @@ export const typeDefs = `#graphql
     id: ID!
     rating: Int!
     comment: String!
+    likes: Int!
     game: Game!
     author: Author!
   }
@@ -20,6 +31,7 @@ export const typeDefs = `#graphql
   type Author {
     id: ID!
     name: String!
+    email: String
     verified: Boolean!
     reviews: [Review!]
   }
@@ -28,8 +40,10 @@ export const typeDefs = `#graphql
     reviews: [Review!]
     games: [Game!]
     authors: [Author!]
+    publishers: [Publisher!]
     game(id: ID!): Game
     author(id: ID!): Author
+    publisher(id: ID!): Publisher
   }
 
   type Mutation {
@@ -38,16 +52,29 @@ export const typeDefs = `#graphql
       description: String
       releaseDate: String
       platforms: [String!]!
+      genre: String
+      publisherId: ID
     ): Game!
     addReview(
       rating: Int!
       comment: String!
       gameId: ID!
       authorId: ID!
+      likes: Int
     ): Review!
     addAuthor(
       name: String!
+      email: String
       verified: Boolean!
     ): Author!
+    addPublisher(
+      name: String!
+      country: String
+      founded: String
+    ): Publisher!
+    deleteGame(id: ID!): Boolean!
+    deleteReview(id: ID!): Boolean!
+    deleteAuthor(id: ID!): Boolean!
+    deletePublisher(id: ID!): Boolean!
   }
 `;
