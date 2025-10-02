46c2588b28c1869beae372c49d1f9f4169a97c4d
Szymon Zych <szymonzych868@gmail.com>
2025-10-02 22:03:12 +0200

    feat: refactor server setup to use express and http server for improved performance

--- GraphQL-brain/index.js
+++ GraphQL-brain/index.js
@@ -1,15 +1,30 @@
 import { ApolloServer } from "@apollo/server";
-import { startStandaloneServer } from "@apollo/server/standalone";
+import { expressMiddleware } from "@apollo/server/express4";
+import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHttpServer";
+import express from "express";
+import http from "http";
+import cors from "cors";
 import { typeDefs } from "./schema.js";
 import { resolvers } from "./resolvers.js";
+import { fileURLToPath } from "url";
+import { dirname, join } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const app = express();
+const httpServer = http.createServer(app);
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
 });
 
-const { url } = await startStandaloneServer(server, {
-  listen: { port: 4000 },
-});
+await server.start();
 
-console.log("Server ready at", url);
+app.use("/", cors(), express.json(), expressMiddleware(server));
+
+app.use(express.static(join(__dirname, "public")));
+
+await new Promise((resolve) => httpServer.listen({ port: 4000 }, resolve));
