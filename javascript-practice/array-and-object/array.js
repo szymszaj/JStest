7596938d21a59580d3d7fa1f1e329dfe3807562f
Szymon Zych <szymonzych868@gmail.com>
2025-10-19 11:57:19 +0200

    fix: correct syntax errors in array.js

--- javascript-practice/array-and-object/array.js
+++ javascript-practice/array-and-object/array.js
@@ -2,10 +2,8 @@ const withDuplicates = [1, 2, 2, 3, 4, 4, 5];
 const unique = [...new set(withDuplicates)];
 console.log(unique);
 
-
 const nested = [[1, 2], [3, 4], [5]];
 const flat = nested.flat();
 
 const deepNested = [1, [2, [3, [4]]]];
-const deepFlat = deepNested.flat(Infinity)
-
+const deepFlat = deepNested.flat(Infinity);
