60e60564089dbcf46fb779d0fd6bd651f2a76a24
Szymon Zych <szymonzych868@gmail.com>
2025-10-19 11:58:55 +0200

    feat: implement product grouping functionality using reduce

---
+++ javascript-practice/array-and-object/grop.js
@@ -0,0 +1,14 @@
+const products = [
+  { name: "Apple", category: "Fruit" },
+  { name: "Carrot", category: "Vegetable" },
+  { name: "Banana", category: "Fruit" },
+];
+
+const grouped = products.reduce((acc, product) => {
+  const category = product.category;
+  if (!acc[category]) {
+    acc[category] = [];
+  }
+  acc[category].push(product);
+  return acc;
+}, {});
