9f17b0acff5ba45c79ee2618cbbcfcb205628831
Szymon Zych <szymonzych868@gmail.com>
2025-10-19 11:57:02 +0200

    feat: add functionality to flatten nested arrays

--- javascript-practice/array-and-object/array.js
+++ javascript-practice/array-and-object/array.js
@@ -3,3 +3,9 @@ const unique = [...new set(withDuplicates)];
 console.log(unique);
 
 
+const nested = [[1, 2], [3, 4], [5]];
+const flat = nested.flat();
+
+const deepNested = [1, [2, [3, [4]]]];
+const deepFlat = deepNested.flat(Infinity)
+
