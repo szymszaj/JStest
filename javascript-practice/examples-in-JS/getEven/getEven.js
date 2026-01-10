fb7173747c642bf53b12d51259a0addf1b24db5e
Szymon Zych <szymonzych868@gmail.com>
2026-01-10 23:53:17 +0100

    update getEven

--- javascript-practice/examples-in-JS/getEven/getEven.js
+++ javascript-practice/examples-in-JS/getEven/getEven.js
@@ -1,3 +1,10 @@
 function getEven(arr) {
-  return arr.filter(num => num % 2 === 0);
+  return arr.filter((num) => num % 2 === 0);
 }
+
+const getEvenNew = (arr) => arr.filter((num) => num % 2 === 0);
+
+console.log(getEven([1, 2, 3, 4, 5, 6]));
+console.log(getEvenNew([1, 2, 3, 4, 5, 6]));
+
+
