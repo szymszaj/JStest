f05e408ff1fca22f8795e1514b218275090f1b16
Szymon Zych <szymonzych868@gmail.com>
2026-01-10 23:51:07 +0100

    feat: add new getPerson

--- javascript-practice/spread-operator-example/index.js
+++ javascript-practice/spread-operator-example/index.js
@@ -14,3 +14,17 @@ const polaczone = [...liczby, ...litery];
 console.log("Liczby:", liczby);
 console.log("Litery:", litery);
 console.log("Połączone używając spread operator:", polaczone);
+
+function getPerson() {
+  return {
+    name: "John",
+    age: 30,
+    city: "New York",
+  };
+}
+
+const person = getPerson();
+const person2 = { ...person, age: 35 };
+
+console.log("Person:", person);
+console.log("Person2:", person2);
