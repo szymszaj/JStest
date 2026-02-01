8b1a155f36c1d04845dd0526f64c5a6454bced64
Szymon Zych <szymonzych868@gmail.com>
2026-02-01 23:03:59 +0100

    feat: Add destructuring functions for displaying person details and extracting first two elements

---
+++ javascript-practice/examples-in-JS/simple-functions/functions-2/destructuring-functions.js
@@ -0,0 +1,16 @@
+function displayPerson({ name, age, city = "Warszawa" }) {
+  return `${name} ma ${age} lat i mieszka w ${city}`;
+}
+
+const person = { name: "Piotr", age: 28, city: "Kraków" };
+console.log(displayPerson(person));
+
+const person2 = { name: "Anna", age: 25 };
+console.log(displayPerson(person2));
+
+function getFirstTwo([first, second]) {
+  return { first, second };
+}
+
+const numbers = [10, 20, 30, 40];
+console.log(getFirstTwo(numbers));
