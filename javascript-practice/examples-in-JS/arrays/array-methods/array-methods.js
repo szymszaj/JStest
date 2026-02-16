bc71067cfedd95cafafb74c8f0a4af837800b2fe
Szymon Zych <szymonzych868@gmail.com>
2026-02-16 22:16:21 +0100

    feat: Add filtering, mapping, and sorting functions for product data

--- javascript-practice/examples-in-JS/arrays/array-methods/array-methods.js
+++ javascript-practice/examples-in-JS/arrays/array-methods/array-methods.js
@@ -24,18 +24,25 @@ const produkty = [
   { id: 5, nazwa: "Krzesło", cena: 600, kategoria: "meble", dostepny: true },
 ];
 
+// 1. Filtruj tylko dostępne produkty
 const dostepneProdukty = produkty.filter((produkt) => produkt.dostepny);
 
+// 2. Pobierz tylko nazwy produktów
 const nazwy = produkty.map((produkt) => produkt.nazwa);
 
+// 3. Znajdź produkty tańsze niż 500 zł
 const tanie = produkty.filter((produkt) => produkt.cena < 500);
 
+// 4. Oblicz całkowitą wartość produktów
 const sumaWartosci = produkty.reduce((suma, produkt) => suma + produkt.cena, 0);
 
+// 5. Sprawdź czy wszystkie produkty są dostępne
 const wszystkieDostepne = produkty.every((produkt) => produkt.dostepny);
 
+// 6. Znajdź pierwszy produkt z kategorii 'meble'
 const mebel = produkty.find((produkt) => produkt.kategoria === "meble");
 
+// 7. Sortuj produkty po cenie (rosnąco)
 const posortowane = [...produkty].sort((a, b) => a.cena - b.cena);
 
 console.log("Dostępne:", dostepneProdukty);
