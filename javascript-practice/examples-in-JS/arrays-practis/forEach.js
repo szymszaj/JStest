543d106b90631bfc800fcfa531878aaad29d154a
Szymon Zych <szymonzych868@gmail.com>
2026-04-19 22:41:44 +0200

    feat: Add user array and log user names with ages

--- javascript-practice/examples-in-JS/arrays-practis/forEach.js
+++ javascript-practice/examples-in-JS/arrays-practis/forEach.js
@@ -1,2 +1,12 @@
 const numbers = [1, 2, 3, 4, 5];
 numbers.forEach((num) => console.log(num * 2));
+
+const users = [
+  { id: 1, name: "Anna", age: 25 },
+  { id: 2, name: "Bob", age: 30 },
+  { id: 3, name: "Cezary", age: 28 },
+];
+
+users.forEach((user) => {
+  console.log(`${user.name} ma ${user.age} lat`);
+});
