1fd65a785c4c9ffa9517caba305e10525a06611d
Szymon Zych <szymonzych868@gmail.com>
2026-07-13 23:24:18 +0200

    feat: Add handleOrder function for calculating total order value

--- javascript-practice/examples-in-JS/function/app-2.js
+++ javascript-practice/examples-in-JS/function/app-2.js
@@ -7,4 +7,7 @@ const calculate = (operation, a, b) => {
 
 console.log("Sum:", calculate(sum, 5, 3));
 
-
+const handleOrder = (product, sell = 0) => {
+  const sum = product.reduce((acc, p) => acc + p.price * p.quantity, 0);
+  return sum - sell;
+};
