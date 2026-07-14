79c8979f1c9bca2a75a70d9f6bccfff96a65e3a6
Szymon Zych <szymonzych868@gmail.com>
2026-07-14 23:02:47 +0200

    feat: Add handleOrderWithDiscount function for calculating total order value with discount

--- javascript-practice/examples-in-JS/function/app-2.js
+++ javascript-practice/examples-in-JS/function/app-2.js
@@ -11,3 +11,30 @@ const handleOrder = (product, sell = 0) => {
   const sum = product.reduce((acc, p) => acc + p.price * p.quantity, 0);
   return sum - sell;
 };
+
+const handleOrderWithDiscount = (product, sell = 0, discount = 0) => {
+  const sum = product.reduce((acc, p) => acc + p.price * p.quantity, 0);
+  return sum - sell - discount;
+};
+
+console.log(
+  "Total Order:",
+  handleOrder(
+    [
+      { price: 10, quantity: 2 },
+      { price: 5, quantity: 3 },
+    ],
+    5,
+  ),
+);
+console.log(
+  "Total Order with Discount:",
+  handleOrderWithDiscount(
+    [
+      { price: 10, quantity: 2 },
+      { price: 5, quantity: 3 },
+    ],
+    5,
+    2,
+  ),
+);
